ced926568c6e3ea55886abedff0f4b64fa6f2714
Estrella1709 <estrellatorrijos17@gmail.com>
2025-03-17 17:25:52 -0600

    Avance de vistas
    Creación de dos plantillas y avance en el home

---
+++ GPISS/resources/views/layouts/header1.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion De proyectos Integradores</title>
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
+    <header class="container">
+        
+        <h1><img id="logo" src="img/CAlogo.png"> Gestión de Proyectos Integradores</h1>
+        <section>
+            <a href="home"><h2 id="home">Acceder</h2></a>
+        </section>
+    </header>
+    <main>
+        @yield('contenido')
+
+       
+    </main>
+
+    <footer>
+        <h5>Universidad Politécnica de Querétaro</h5>
+        <h5>Ingeniería en Sistemas Computacionales</h5>
+    </footer>
+    
+</body>
+</html>
